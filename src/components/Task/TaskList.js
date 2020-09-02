466ead02c466aa6e1bfeddb03fe91614fd7a7d4d
danbiilee <danbi.db@gmail.com>
2020-09-02 22:18:24 +0900

    [FEAT] #2 : TaskList Context API 적용
    TaskList, TaskList의 Title에 적용함.

--- src/components/Task/TaskList.js
+++ src/components/Task/TaskList.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Title from './Title';
 import Task from './Task';
 import './TaskList.scss';
+import { useTaskState } from '../../reducers/TaskContext';
 
 const TaskList = () => {
+  const tasks = useTaskState();
   return (
     <>
       <section className="TaskList">
         <Title />
         <div className="task-wrapper">
-          <Task />
-          <Task />
-          <Task />
+          {tasks.map(task => (
+            <Task key={task.id} task={task} />
+          ))}
         </div>
       </section>
     </>
--- src/components/Task/Title.js
+++ src/components/Task/Title.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { TiPencil, TiPlus, TiTimes } from 'react-icons/ti';
 import './Title.scss';
+import { useTaskState } from '../../reducers/TaskContext';
 
 const Title = () => {
+  const tasks = useTaskState();
+  const undoneCnt = tasks.reduce((acc, cur) => acc + !cur.isDone, 0);
   return (
     <>
       <header className="Title">
@@ -13,7 +16,7 @@ const Title = () => {
           <TiTimes />
         </div>
       </header>
-      <div className="tasklist-doing">진행 중인 업무 2개</div>
+      <div className="tasklist-doing">진행 중인 업무 {undoneCnt}개</div>
     </>
   );
 };
