3960e27633a737aad47dcf82110651ca8324a4be
danbiilee <danbi.db@gmail.com>
2020-09-03 22:11:13 +0900

    [FEAT] #2 : Point에 state값 적용

--- src/components/Point/Point.js
+++ src/components/Point/Point.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Point.scss';
 
-const Point = () => {
-  return (
-    <div className="Point">
-      <span className="isFull"></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  );
+const renderSpan = point => {
+  const spanList = [];
+  if (point) {
+    for (let i = 0; i < 5; i++) {
+      if (i < point) spanList.push(<span key={i} className="isFull"></span>);
+      else spanList.push(<span key={i}></span>);
+    }
+    return spanList;
+  } else return null;
+};
+
+const Point = ({ point }) => {
+  const spanList = renderSpan(point);
+  return <div className="Point">{spanList && spanList.map(item => item)}</div>;
 };
 
 export default Point;
