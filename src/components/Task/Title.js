5108421086578f2b0b290aa29c3b32319bb89489
danbiilee <danbi.db@gmail.com>
2020-09-07 23:16:16 +0900

    [STYLE] #1 : Task CREATE 영역 마크업/SCSS 추가

--- src/components/Task/Title.js
+++ src/components/Task/Title.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TiPencil, TiPlus, TiTimes } from 'react-icons/ti';
+import { MdGroupAdd, MdLocalOffer } from 'react-icons/md';
+import { ImCalendar } from 'react-icons/im';
 import './Title.scss';
-import { useTaskState } from '../../reducers/TaskContext';
+import {
+  useTaskState,
+  useTaskNextId,
+  useTaskDispatch,
+} from '../../reducers/TaskContext';
 
 const Title = () => {
+  const dispatch = useTaskDispatch();
   const tasks = useTaskState();
+  const nextId = useTaskNextId();
+  const [isActive, setIsActive] = useState(false);
   const undoneCnt = tasks.reduce((acc, cur) => acc + !cur.isDone, 0);
+
+  const onToggle = () => {
+    setIsActive(!isActive);
+  };
+  const onCreate = () => {};
+
   return (
     <>
       <header className="Title">
         <h3>메인페이지 분리하기</h3>
         <div className="btn-wrapper">
           <TiPencil />
-          <TiPlus />
+          <TiPlus onClick={onToggle} />
           <TiTimes />
+        </div>
+        <div className="create-wrapper">
+          <textarea placeholder="새 업무 만들기"></textarea>
+          <div className="cr-btn-wrapper">
+            <div className="cr-btn-l">
+              <MdGroupAdd />
+              <MdLocalOffer />
+              <ImCalendar />
+            </div>
+            <div className="cr-btn-r">
+              <button className="btn-cancel">취소</button>
+              <button className="btn-create">만들기</button>
+            </div>
+          </div>
         </div>
       </header>
       <div className="tasklist-doing">진행 중인 업무 {undoneCnt}개</div>
