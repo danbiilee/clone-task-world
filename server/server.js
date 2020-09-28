eba4f4336addbe6396240aa4f1d2f0f4dfe01df2
danbiilee <danbi.db@gmail.com>
2020-09-28 20:13:16 +0900

    [FEAT] #8: MongoDB 접속

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
 
-// bodyParser를 사용하기 위한 설정
+// DB 설정
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Conneced to mongoDB server');
+});
+db.on('error', err => {
+  console.log('DB ERROR: ', err);
+});
+
+// bodyParser 설정
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// 라우터 설정
 const router = require('./routes')(app); // 라우터
-const port = process.env.PORT || 4000; // 서버 포트
 
-// 서버 실행
-//const server = app.listen(port, () => console.log(`Express server has started on port ${port}`));
+// 포트 설정
+const port = process.env.PORT || 4000; // 서버 포트
 app.listen(port, () =>
   console.log(`Express server has started on port ${port}`),
 );
